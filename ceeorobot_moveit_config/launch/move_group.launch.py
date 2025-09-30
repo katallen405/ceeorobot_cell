4cb4992e223b893168ed94f03459f3d53374fcf2
Kat Allen <katallen@gmail.com>
2025-09-30 10:00:54 -0400

    description should be good, moveit config is a mess

--- ceeorobot_moveit_config/launch/move_group.launch.py
+++ ceeorobot_moveit_config/launch/move_group.launch.py
@@ -1,7 +1,209 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_move_group_launch
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+import xacro
+
+
+def load_file(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return file.read()
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        import yaml
+
+        with open(absolute_file_path, "r") as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("ceeorobot", package_name="ceeorobot_moveit_config").to_moveit_configs()
-    return generate_move_group_launch(moveit_config)
+    # Command-line arguments
+    db_arg = DeclareLaunchArgument(
+        "db", default_value="false", description="Database flag"
+    )
+    
+    use_sim_time = LaunchConfiguration("use_sim_time", default="false")
+
+    xacro_file = PathJoinSubstitution([
+        FindPackageShare("ceeorobot_description"),
+        "urdf",
+        "ceeorobot_cell.urdf.xacro"
+    ])
+
+    # Process the URDF file
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    robot_description_config = doc.toxml()
+    robot_description = {"robot_description": robot_description_config}
+
+    # Robot Semantic Description
+    robot_description_semantic_config = load_file(
+        "ceeorobot_moveit_config", "config/ceeorobot.srdf"
+    )
+    robot_description_semantic = {
+        "robot_description_semantic": robot_description_semantic_config
+    }
+
+    # Kinematics.yaml file:
+    kinematics_yaml = load_yaml("ceeorobot_moveit_config", "config/kinematics.yaml")
+    robot_description_kinematics = {"robot_description_kinematics": kinematics_yaml}
+
+    # Planning Functionality
+    ompl_planning_pipeline_config = {
+        "move_group": {
+            "planning_plugin": "ompl_interface/OMPLPlanner",
+            "request_adapters": """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/ResolveConstraintFrames default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""",
+            "start_state_max_bounds_error": 0.1,
+        }
+    }
+    ompl_planning_yaml = load_yaml("ceeorobot_moveit_config", "config/ompl_planning.yaml")
+    ompl_planning_pipeline_config["move_group"].update(ompl_planning_yaml)
+
+    # Trajectory Execution Functionality
+    controllers_yaml = load_yaml("ceeorobot_moveit_config", "config/ros2_controllers.yaml")
+    
+    moveit_controllers = {
+        "moveit_simple_controller_manager": controllers_yaml,
+        "moveit_controller_manager": "moveit_simple_controller_manager/MoveItSimpleControllerManager",
+    }
+
+    trajectory_execution = {
+        "moveit_manage_controllers": True,
+        "trajectory_execution.allowed_execution_duration_scaling": 1.2,
+        "trajectory_execution.allowed_goal_duration_margin": 0.5,
+        "trajectory_execution.allowed_start_tolerance": 0.01,
+    }
+
+    planning_scene_monitor_parameters = {
+        "publish_planning_scene": True,
+        "publish_geometry_updates": True,
+        "publish_state_updates": True,
+        "publish_transforms_updates": True,
+    }
+
+    # Joint limits
+    joint_limits_yaml = {
+        "robot_description_planning": load_yaml(
+            "ceeorobot_moveit_config", "config/joint_limits.yaml"
+        )
+    }
+
+    # Start the actual move_group node/action server
+    move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[
+            robot_description,
+            robot_description_semantic,
+            kinematics_yaml,
+            ompl_planning_pipeline_config,
+            trajectory_execution,
+            moveit_controllers,
+            planning_scene_monitor_parameters,
+            joint_limits_yaml,
+            {"use_sim_time": use_sim_time},
+        ],
+    )
+
+    # RViz
+    rviz_config_file = PathJoinSubstitution([
+        FindPackageShare("ceeorobot_moveit_config"),
+        "config",
+        "moveit.rviz"
+    ])
+    
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2_moveit",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        parameters=[
+            robot_description,
+            robot_description_semantic,
+            ompl_planning_pipeline_config,
+            kinematics_yaml,
+            joint_limits_yaml,
+        ],
+    )
+
+    # Static TF
+    static_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_transform_publisher",
+        output="log",
+        arguments=["--frame-id", "world", "--child-frame-id", "world"],
+    )
+
+    # Publish TF
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
+    # ros2_control using FakeSystem as hardware
+    ros2_controllers_path = os.path.join(
+        get_package_share_directory("ceeorobot_moveit_config"),
+        "config",
+        "ros2_controllers.yaml",
+    )
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, ros2_controllers_path],
+        output="both",
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    ur3_arm_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["ur3_arm_controller", "-c", "/controller_manager"],
+    )
+
+    gripper_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["gripper_controller", "-c", "/controller_manager"],
+    )
+
+    return LaunchDescription(
+        [
+            db_arg,
+            static_tf,
+            robot_state_publisher,
+            ros2_control_node,
+            joint_state_broadcaster_spawner,
+            ur3_arm_controller_spawner,
+            gripper_controller_spawner,
+            move_group_node,
+            rviz_node,
+        ]
+    )
