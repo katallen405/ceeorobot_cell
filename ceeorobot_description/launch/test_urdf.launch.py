fd3dc869cb988444b161d9a2cc9f834e07bbd4c9
Kat Allen <katallen@gmail.com>
2026-01-15 16:50:35 -0500

    working ceeo robot cell

---
+++ ceeorobot_description/launch/test_urdf.launch.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    
+    # Get URDF via xacro
+    urdf_file = '/tmp/ceeorobot_debug.urdf'
+    
+    with open(urdf_file, 'r') as infp:
+        robot_desc = infp.read()
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_desc}],
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            output='screen',
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            arguments=['-d', os.path.join(get_package_share_directory('ceeorobot_description'), 'config', 'view_robot.rviz')],
+        ),
+    ])
